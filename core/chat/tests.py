fb7f67b7d8fe3e8508c4efd1172c1af9c86706e8
NJ9802 <nelsonjavier9802@gmail.com>
2022-11-16 15:01:27 -0500

    Automatic Tests added

--- core/chat/tests.py
+++ core/chat/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
+from django.urls import reverse
+
 
 # Create your tests here.
+
+class InspectLoginPage(TestCase):
+    def test_status_code(self):
+        '''Checking if Status Code on Login Page == 200 '''
+
+        login = self.client.get(reverse('login'))
+
+        self.assertEqual(login.status_code, 200)
